3fbb63cad7dcadde6f5582ee9efe91c5be08c4c3
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-13 12:05:14 -0300

    Using Bootstrap.

--- 1-my-first-app/src/components/SayHi.js
+++ 1-my-first-app/src/components/SayHi.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default function SayHi(props) {
     return (
         <>
             <h2>Hola { props.userInfo.name }</h2>
 
-            <button onClick={ () => props.sayHi(props.userInfo.name, props.userInfo.age) }>Say Hi</button>
+            <Button 
+                onClick={ () => props.sayHi(props.userInfo.name, props.userInfo.age) }
+                variant="primary"
+            >Say Hi</Button>
         </>
     );
 }
