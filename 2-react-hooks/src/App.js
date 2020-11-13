2e9a8267e2bf72129f61ec64bb55c65e043a29c1
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-13 15:08:20 -0300

    useState and useEffect.

--- 2-react-hooks/src/App.js
+++ 2-react-hooks/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [stateCar, setStateCar] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    
+    return () => {
+      
+    }
+  }, [count]);
+
+  const encenderApagar = () => {
+    setStateCar(!stateCar);
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h3>The car is: { stateCar ? 'on' : 'off'}</h3>
+
+        <button onClick={ encenderApagar }>Encender/Apagar</button>
+
+        <h4>Clicks: { count }</h4>
       </header>
     </div>
   );
