f04303e60218e489502de44c270a9816c381dfd0
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-16 20:01:05 -0300

    Using styles instead of css. I moved the style of the Header component to it's own file.

--- 4-components/src/components/Header.jsx
+++ 4-components/src/components/Header.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 
+const styles = {
+    header: {
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white'
+    }
+}
+
 export default class Header extends Component {
     render() {
         const { text, handleClick } = this.props;
 
         return (
-            <header className="App-header">
+            <header style={ styles.header }>
                 <img onClick={ handleClick } src={ logo } className="App-logo" alt="logo" />
                 <p>
                 { text }
